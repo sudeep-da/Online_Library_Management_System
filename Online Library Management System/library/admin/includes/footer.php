8c37d01d8c5cd54e9e3cbea1bd47fe8ba2a50113
sudeep-da <ashtekardsudeep@gmail.com>
2025-03-12 11:45:35 +0530

    Files

---
+++ Online Library Management System/library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : Mahantesh & M.Jameer</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
